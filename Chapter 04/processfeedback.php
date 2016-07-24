ddc28cb7cd009d9c708ffb19092fe1e2e0c0acc0
liuxiaoyue1203 <liuxiaoyue1203@163.com>
2016-07-24 19:38:24 +0800

    Add chapter04

---
+++ Chapter 04/processfeedback.php
@@ -0,0 +1,30 @@
+<?php
+//create short variable names
+$name=trim($_POST['name']);
+$email=trim($_POST['email']);
+$feedback=trim($_POST['feedback']);
+
+//set up some static information
+$toaddress = "feedback@example.com";
+
+$subject = "Feedback from web site";
+
+$mailcontent = "Customer name: ".$name."\n".
+			   "Customer email: ".$email."\n".
+               "Customer comments:\n".$feedback."\n";
+
+$fromaddress = "From: webserver@example.com";
+
+//invoke mail() function to send mail
+mail($toaddress, $subject, $mailcontent, $fromaddress);
+
+?>
+<html>
+<head>
+<title>Bob's Auto Parts - Feedback Submitted</title>
+</head>
+<body>
+<h1>Feedback submitted</h1>
+<p>Your feedback has been sent.</p>
+</body>
+</html>
